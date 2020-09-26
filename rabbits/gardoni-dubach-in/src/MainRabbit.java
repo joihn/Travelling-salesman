0ba56dadbe5808a4f3f8a46342a3d37445b79afc
Maxime G <maxime.gardoni@epfl.ch>
2020-09-26 21:50:30 +0200

    testing the new git architecture

--- rabbits/gardoni-dubach-in/src/MainRabbit.java
+++ rabbits/gardoni-dubach-in/src/MainRabbit.java
@@ -1,7 +1,7 @@
 public class MainRabbit {
 
     public static void main(String[] args){
-
+        System.out.println("testing the new git architecture");
         RabbitsGrassSimulationModel.main(args);
 
     } 
