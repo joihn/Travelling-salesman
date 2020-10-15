3c09e2222576eec1e334a137e6862bd63f932700
Maxime G <maxime.gardoni@epfl.ch>
2020-10-15 16:38:19 +0200

    initial draw of astar

--- deliberative/263557-271088-ex2/src/ASTAR.java
+++ deliberative/263557-271088-ex2/src/ASTAR.java
@@ -1,14 +1,27 @@
+import logist.plan.Plan;
 import logist.simulation.Vehicle;
+import logist.task.Task;
+import logist.task.TaskDistribution;
 import logist.task.TaskSet;
+import logist.topology.Topology;
 import logist.topology.Topology.City;
+import logist.plan.Action;
+import logist.task.TaskSet;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Collections;
 
-public class ASTAR {
-    Vehicle vehicle;
-    City initialCity;
+public class ASTAR{
+    Plan optimalPlan ;
 
-    public ASTAR(City initialCity_, Vehicle vehicle_, TaskSet taskSet_){
+    public ASTAR(Vehicle vehicle_, TaskSet taskSet_){
         this.initialCity = initialCity_;
+        State initialNode= new State(vehicle_.getCurrentCity(),vehicle_,vehicle_.getCurrentTasks(), taskSet_, null, null);
 
+        ArrayList<State> finalNodes = new ArrayList<State>();
+        ArrayList<State> C = new ArrayList<State>();
 
 
     }
