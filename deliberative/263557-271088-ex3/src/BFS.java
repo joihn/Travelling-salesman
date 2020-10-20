fe09f6768ca4259ec460085edd5585cd4ba664fe
marceldubach <marcel.dubach@epfl.ch>
2020-10-20 15:36:31 +0200

    final version for assignment

--- deliberative/263557-271088-ex3/src/BFS.java
+++ deliberative/263557-271088-ex3/src/BFS.java
@@ -14,6 +14,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Collections;
+
 public class BFS {
     Plan optimalPlan ;
     State optimalFinalNode;
