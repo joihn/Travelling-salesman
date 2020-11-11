2d68a37a254484dac66b3172eff6ae81c0d653ae
Maxime G <maxime.gardoni@epfl.ch>
2020-11-11 13:08:36 +0100

    added file from ex4 in ex5

---
+++ 5-auction/263557-271088-ex5/src/ExTask.java
@@ -0,0 +1,19 @@
+import logist.task.Task;
+
+public class ExTask {
+
+    public enum ActionType{
+        PICKUP,
+        DELIVERY
+    }
+    public Task task;
+    public ActionType actionType;
+
+    public ExTask(Task task_, ActionType actionType_ ){
+        this.task=task_;
+        this.actionType=actionType_;
+    }
+
+
+
+}
